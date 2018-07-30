c60f968c9fcad971d671b84a9beba386525e7844
IT-VRBOX3\Nick <nsaunderswps@gmail.com>
2018-07-30 11:28:26 -0400

    updates

---
+++ Assets/Scripts/Tests/Resume.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resume : MonoBehaviour {
+
+	/*
+
+	Augmented Reality: Android/iOS/UWP/Hololens
+		Target based AR tracking with Vuforia API for Unity
+		Design discreet AR targets with a five star tracking rating
+		Environmental based AR tracking with ARCore API and Unity
+		Hone user interaction and interface for AR platforms		
+	Virtual Reality: Vive/Oculus
+		Design room scale and seated VR experiences
+		Create intuitive user input systems
+		Tune and refine driving simulator 
+	Mobile: Android/iOS
+		Optimize performance for multipulgenerations of devices
+		Build and deploy to the Google Playstore
+	Stand Alone: Windows/Mac 
+		Interactive story driven game
+		Mechanic driven game and level design
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 */
+}
