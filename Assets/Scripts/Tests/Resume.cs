92a413019984e1464836ef751034c2c1956d031b
IT-VRBOX3\Nick <nsaunderswps@gmail.com>
2018-07-31 12:03:09 -0400

    resume update

--- Assets/Scripts/Tests/Resume.cs
+++ Assets/Scripts/Tests/Resume.cs
@@ -23,8 +23,29 @@ public class Resume : MonoBehaviour {
 		Mechanic driven game and level design
 
 
+	Welcome Center Intercative AR - Unity and Vuforia API
+		Target based AR image tracking
+		Helped design a five star tracking rating target that was seamlessly integrated into a map
+		created an intuitive user interface and refined user interaction
+		optimized performance for the iOS, Android, and UWP platforms
 
+	Neighbors VR - Unity and SteamVR API
+		Room scale virtual reality for the HTC Vive
+		Designed for two players on one Vive system
+		created a four slot inventorty system
+		Crafting system to combine object and create new ones
+		From scratch teleporting system for each player
 
+	Racing Simulator VR - Unity and Oculus API
+		Integrate VR into a realistic car physics plugin for unity
+		Checkpoint and mini-map system to keep the user on the track and insure they are going the correct way
+		Record the lowest times in minutes, seconds, and miliseconds and post them on a local leaderboard
+
+	Interactive Hololens demo - UNity and Microsoft Hololens API
+		Enviromental based AR tracking with the Hololens depth mapping
+		Hololens gesture recognition for user input and control
+		Remote control car inAR with input from a bluetooth controller
+		Major performace optimization for the Hololens hardware
 
 
 
